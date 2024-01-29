1e9d751af6338f43e27fe5ea6181758c525a0269
bhargav bhatt <bhargavmbhatt24@gmail.com>
2024-01-29 22:31:58 +0530

    i have error of cors in angular so updated that

--- userdata.php
+++ userdata.php
@@ -1,4 +1,8 @@
 <?php
+    header("Access-Control-Allow-Origin: http://localhost:4200");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Credentials: true");
     header('Content-Type: application/json');
     $arr=array();
     include "db.php";
